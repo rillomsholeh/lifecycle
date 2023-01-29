56f8dac375bf2697211e87ec4457dea9e254fd90
unknown <rillomsholeh>
2023-01-30 01:30:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wheather: null
+    };
+  }
+
+  fetchData() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((obj) => {
+        //console.log('javascript object: ', obj)
+        // this.setState({ wheather: obj });
+      });
+  }
+  
+  componentDidMount() {
+    console.log("get api compnents Did Mount");
+    this.fetchData();
+  }
+ render() {
+    return (
+      <div>
+        {this.state.wheather
+          && <div key={this.state.wheather.city_info.name}>{this.state.wheather.city_info.name}</div>
+        }
+      </div>
+    )
+  }
 }
+
 
 export default App;

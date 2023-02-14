16bd96451814e0a634fd1d62e68eaf1047c994f0
unknown <rillomsholeh>
2023-02-15 04:59:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,65 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    users: []
-  };
+    searchTerm: '',
+    news: [],
+  }
 
-  
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => {
-        this.setState({
-          users
-        });
+    const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=58dbe4aef1424596a06fa3e07d288cec`;
+
+    axios.get(url)
+      .then(res => {
+        this.setState({ news: res.data.articles });
+      })
+      .catch(err => {
+        console.log(err);
       });
   }
 
-  
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = `https://newsapi.org/v2/top-headlines?sources=techcrunch${this.state.searchTerm}&apiKey=58dbe4aef1424596a06fa3e07d288cec`;
+
+    axios.get(url)
+      .then(res => {
+        this.setState({ news: res.data.articles });
+      })
+  }
+
   render() {
     return (
-      <div>
-        <ul>
-          {this.state.users.map((user, index) => (
-            <li key={index}>
-              <p>Id:{user.id}</p>
-              <p>Nama: ({user.name})</p>
-              <p>userName: {user.username}</p>
-              <p>Email: ({user.email})</p>
-            </li>
-          ))}
-        </ul>
+        <div>
+		      <div style={{marginLeft:'980px', marginBottom:'70px', marginTop:'30px'}}>
+                <form
+                    className="input-group search" 
+                    onSubmit={this.handleSubmit}>
+                    <input className="form-control input" type="text" value={this.state.searchTerm} onChange={this.handleChange} />
+                    <button 
+                        className="btn btn-primary"
+                        type="submit">Submit</button>
+                </form>
+            </div>
+
+        {this.state.news.map((article, id) => (
+          <div className="text-center mt-3" key={id}>
+              <img style={{height:'260px', marginLeft:'400px'}} src={article.urlToImage} alt="news" />
+             <h2 style={{textAlign: 'center'}}>{article.title}</h2>
+             <p style={{textAlign:'center'}}>{article.description}</p>
+          </div>
+        ))}
       </div>
     );
   }
